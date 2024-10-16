2bd5f73d3643312cf61a180a4a58320c7759be11
Konstantin <k.ognev709@mail.ru>
2024-10-16 13:53:32 +0600

    Добавлена возможность поиска всех или только одного коктеля

--- backend/routes/CocktailRouter.ts
+++ backend/routes/CocktailRouter.ts
@@ -4,6 +4,17 @@ import Cocktail from '../models/Cocktails';
 const cocktailRouter = express.Router();
 cocktailRouter.use(express.json());
 
+cocktailRouter.get('/', async (req, res) => {
+  const findAllCocktails = await Cocktail.find()
+  res.send(findAllCocktails)
+})
+
+cocktailRouter.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const findCocktail = await Cocktail.find({ _id: id })
+  res.send(findCocktail)
+})
+
 cocktailRouter.post('/' , async (req, res, next) => {
   try {
     const cocktail = new Cocktail({

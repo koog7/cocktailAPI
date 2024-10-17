6ee2e03e4524f9244b75e9860a9c9313a4b39b86
Konstantin <k.ognev709@mail.ru>
2024-10-17 16:57:44 +0600

    Добавлен прелоудер и not found страница

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,42 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import Home from './containers/Home';
 import Navbar from './components/Navbar';
 import Login from './containers/Auth/Login.tsx';
 import Registration from './containers/Auth/Registration.tsx';
 import MyCocktails from './containers/MyCocktails.tsx';
+import NotFound from './components/NotFound.tsx';
+import { useSelector } from 'react-redux';
+import { RootState } from './app/store.ts';
 
 const App = () => {
+  const loader = useSelector((state: RootState) => state.Cocktail.loader)
+
   return(
     <>
       <div>
+        <div id="loader-container" style={{ display: loader ? 'block' : 'none' }}>
+          <div className="loader"></div>
+        </div>
         <div>
           <Navbar />
         </div>
         <Routes>
           <Route path="/" element={(
-            <Home/>
-          )}/>
+              <Home />
+          )} />
           <Route path="/login" element={(
-            <Login/>
-          )}/>
+              <Login />
+          )} />
           <Route path="/registration" element={(
-            <Registration/>
-          )}/>
+              <Registration />
+          )} />
           <Route path="/myCocktails" element={(
-              <MyCocktails/>
-          )}/>
+              <MyCocktails />
+          )} />
+          <Route path="*" element={(
+              <NotFound />
+          )} />
         </Routes>
       </div>
     </>
